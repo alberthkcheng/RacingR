23fb826978903b87205972732bad489b5ee61b93
Albert HK Cheng <albert.hk.cheng@gmail.com>
2017-10-08 14:51:13 +0800

    update odds calculation

--- R/odds.R
+++ R/odds.R
@@ -15,9 +15,25 @@ odds_to_prob <- function(odds){
   return(prob / sum(prob))
 }
 
-prob_to_odds <- function(prob){
-  return(1 / prob)
+#'
+#' @export
+#'
+prob_to_odds <- function(prob, return_rate = 0.825, type = c("win", "q")){
+  type <- match.arg(type)
+  if(type == "win"){
+    return(return_rate / prob)
+  }else if(type == "q"){
+    probs <- outer(prob,prob) * 4
+    for(i in 1:length(prob)){
+      for(j in 1:i){
+        probs[i,j]<-0
+      }
+    }
+    return(return_rate / probs)
+  }
+  return(return_rate / prob)
 }
+
 odds_to_pool <- function(odds, total_pool = 1000000, return_rate = 0.825){
   pool <- 1 / odds
   return(pool / sum(pool) * total_pool / return_rate)
